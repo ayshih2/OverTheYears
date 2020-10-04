357be0ac2e7412787eb685f9abd8daf7303505a1
Annabelle Shih <annabelleshih@gmail.com>
2020-10-04 00:30:14 -0700

    using spotify, azure, and musixmatch APIs

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 ## OverTheYears
-_**Still in progress!**_<br>
+**Status:** In progress!<br>
 A deep dive into an artist's discography and the associated audio features for each song of each album.
 
 ## Motivation
 An exercise in data visualization. Wanted practice using different programming languages (Javascript) and libraries (Pandas for Python) and to try my hand at implementing scrollytelling, an online visual storytelling experience, after seeing articles from [The Pudding](https://pudding.cool).
 
 ## Installation & Setup
+### Get API Keys
+1. [Spotify](https://developer.spotify.com/dashboard/login)
+  - To get albums, tracks and audio features for an artist.
+  - Store credentials as environment variables called `CLIENT_ID` and `CLIENT_SECRET`.
+1. [MusixMatch](https://developer.musixmatch.com)
+  - For getting the lyrics of each track.
+  - Store api key as environment variable called `MUSIXMATCH_KEY`.
+1. [Microsoft Azure](https://azure.microsoft.com/en-us/free/cognitive-services/)
+  - For using their Text Analytics library, which supports multiple languages including English and Korean.
+  - Store key and endpoint as `AZURE_KEY` and `AZURE_ENDPOINT` respectively.
+
+### Run Program
 1. First install pipenv if needed as shown [here](https://pypi.org/project/pipenv/).
 1. Clone repository and navigate into it.
 1. Install project dependancies:<br>
 `pipenv install`
 1. Activate project's virtual environment:<br>
 `pipenv shell`
-1. Go through Spotify For Developers to get client credentials and store it as environmental variables `CLIENT_ID` and `CLIENT_SECRET`.
 1. This particular project looks at BTS' discography; if you want to get another artist's discography, replace the `ARTIST_ID` variable's value with a different artist's ID.
   1. Get artist ID by opening Spotify > Going to the artist's page > Clicking the ellipsis button > Share > Copy Spotify URI
   1. Everything after "spotify:artist:" is the ID for that particular artist.
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 import pandas as pd
 import requests
 import json
@@ -11,7 +13,7 @@ in a file. Must register at Spotify for Developers first for it to work properly
 Used following article as a starting point: https://stmorse.github.io/journal/spotify-api.html
 """
 def get_data():
-    # (1) Get access token
+    # (1) GET ACCESS TOKEN
     AUTH_URL = 'https://accounts.spotify.com/api/token'
     client_id = os.environ.get('CLIENT_ID')
     client_secret = os.environ.get('CLIENT_SECRET')
@@ -23,20 +25,19 @@ def get_data():
     try:
         auth_response.raise_for_status()
     except requests.exceptions.HTTPError as e:
-        # Whoops it wasn't a 200
         print("Error when trying to get access token")
         raise
     auth_response_data = auth_response.json()
     access_token = auth_response_data['access_token']
 
-    # (2) MAKE API CALLS FOR DATA - BTS
+    # (2) MAKE API CALLS FOR DATA 
     headers = {
         'Authorization': 'Bearer {token}'.format(token=access_token)
     }
     BASE_URL = 'https://api.spotify.com/v1/'
-    ARTIST_ID = '3Nrfpe0tUJi4K4DXYWgMUX'
+    ARTIST_ID = '3Nrfpe0tUJi4K4DXYWgMUX' # BTS' ID
 
-    # Get all albums
+    # Get all albums for artist
     res = requests.get(BASE_URL + 'artists/{id}/albums'.format(id=ARTIST_ID), 
                         headers=headers,
                         params={'include_groups': 'album', 'limit': 50, 'country': 'SK'})
@@ -46,17 +47,26 @@ def get_data():
         print("Error when trying to retrieve albums")
         raise
 
+    albums_to_exclude = ['Skool Luv Affair (Special Edition)', 'Wake Up (Standard Edition)', 'Youth', 'FACE YOURSELF', 'MAP OF THE SOUL : 7 ~ THE JOURNEY ~']
     data = []
     # Get tracks in each album
     for album in res.json()['items']:
-        tracks = requests.get(BASE_URL + 'albums/{id}/tracks'.format(id=album['id']), headers=headers)
+        if album['name'] in albums_to_exclude:
+            # Only want Korean albums - not Japanese or special editions
+            continue
+        album_info = requests.get(BASE_URL + 'albums/{id}'.format(id=album['id']), headers=headers)
         try:
-            tracks.raise_for_status()
+            album_info.raise_for_status()
         except requests.exceptions.HTTPError as e:
-            print("Error when trying to retrieve tracks for the album: {album}".format(album=album['name']))
-            raise
+            print("Error when trying to retrieve further album details for the album: {album}".format(album=album['name']))
+            raise        
+        album_info = album_info.json()
 
-        for track in tracks.json()['items']:
+        for track in album_info['tracks']['items']:
+            if track['name'].lower().find('skit') != -1:
+                # skip over skits, get only music
+                continue
+            
             # Get popularity
             track_info = requests.get(BASE_URL + 'tracks/{id}'.format(id=track['id']), headers=headers) 
             try:
@@ -78,10 +88,11 @@ def get_data():
             # Add extra details for future reference
             all_details_for_track.update({
                 'track_name': track['name'],
+                'track_popularity': track_info['popularity'],
                 'album_name': album['name'],
-                'release_date': album['release_date'],
+                'album_release_date': album['release_date'],
                 'album_id': album['id'],
-                'popularity': track_info['popularity']
+                'album_popularity': album_info['popularity']
             })
             data.append(all_details_for_track)
 
@@ -89,11 +100,76 @@ def get_data():
     with open(DISCOGRAPHY_FILE_NAME, 'w') as f:
         json.dump(data, f)
 
+def authenticate_client():
+    ta_credential = AzureKeyCredential(os.environ.get('AZURE_KEY'))
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=os.environ.get('AZURE_ENDPOINT'), 
+            credential=ta_credential)
+    return text_analytics_client
+
+def get_lyrics(client):
+    BASE_URL = 'https://api.musixmatch.com/ws/1.1/'
+    API_KEY = os.environ.get('MUSIXMATCH_KEY')
+    GET_TRACK_BASE_URL = 'matcher.track.get?format=json&callback=callback&q_artist=bts&q_track={song}&apikey={api_key}'.format(song='springday', api_key=API_KEY)
+
+    q_track = 'q_track=dynamite'
+    res = requests.get(BASE_URL + GET_TRACK_BASE_URL)
+    data = res.json()
+    id = data['message']['body']['track']['commontrack_id']
+    #print(data['message']['body']['track']['commontrack_id'])
+
+    GET_LYRICS_URL = 'matcher.lyrics.get?format=json&callback=callback&q_artist=bts&q_track={song}&apikey={api_key}'.format(song='truth untold', api_key=API_KEY)
+
+    res = requests.get(BASE_URL + GET_LYRICS_URL)
+    lyrics = res.json()['message']['body']['lyrics']['lyrics_body']
+    lyrics = lyrics.replace('\n\n', ' ').replace('\n', ' ')
+    lyrics = lyrics[:lyrics.find(' ...')]
+    print(lyrics)
+
+    # https://docs.microsoft.com/en-us/azure/cognitive-services/text-analytics/quickstarts/text-analytics-sdk?tabs=version-3-1&pivots=programming-language-python
+    response = client.analyze_sentiment(documents=[lyrics])[0]
+    print("Document Sentiment: {}".format(response.sentiment))
+    print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
+        response.confidence_scores.positive,
+        response.confidence_scores.neutral,
+        response.confidence_scores.negative,
+    ))
+    for idx, sentence in enumerate(response.sentences):
+        print("Sentence: {}".format(sentence.text))
+        print("Sentence {} sentiment: {}".format(idx+1, sentence.sentiment))
+        print("Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
+            sentence.confidence_scores.positive,
+            sentence.confidence_scores.neutral,
+            sentence.confidence_scores.negative,
+        ))
+        print(idx)
+
 if __name__ == "__main__":
     # Uncomment if you need to get discography data first
-    # get_data()
+    #get_data()
 
     # Convert to list of dicts to DataFrame (2D table with labeled axes) 
     df = pd.read_json(DISCOGRAPHY_FILE_NAME)
-    print(df.head())
-    print(df.columns)
+    albums = df.album_name.unique()
+
+    audio_feature_titles = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'valence', 'tempo']
+    total_songs = 0
+    total_albums = 0
+    for album in albums:
+        print(album)
+        total_albums += 1
+        tracks = df.loc[df['album_name'] == album]
+        total_songs += len(tracks.index)
+        #print(tracks)
+
+    print("There are {} songs".format(total_songs))
+
+    # Use Microsoft Azure's Text Analytics library
+    #client = authenticate_client()
+    #get_lyrics(client)
+    
+    #print(tracks[['track_name', 'danceability', 'valence', 'energy']])
+    #audio_features_df = (df.ix['MAP OF THE SOUL : 7 ~ THE JOURNEY ~'])[['danceability', 'energy', 'track_name']]
+    #audio_features_df = df.loc[[df['album_name'] == 'MAP OF THE SOUL : 7 ~ THE JOURNEY ~'], ['danceability', 'energy', 'track_name']]
+    #print(audio_features_df)
+
